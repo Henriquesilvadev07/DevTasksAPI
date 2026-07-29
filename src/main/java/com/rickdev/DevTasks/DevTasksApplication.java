d5da1768f7c135e64212fe8949f0a792a82af313
RickDev <henriiisilva2007@gmail.com>
2026-07-28 21:40:47 -0300

    Primeiro commit

---
+++ src/main/java/com/rickdev/DevTasks/DevTasksApplication.java
@@ -0,0 +1,13 @@
+package com.rickdev.DevTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rickdev/DevTasks/DevTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickdev.DevTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
