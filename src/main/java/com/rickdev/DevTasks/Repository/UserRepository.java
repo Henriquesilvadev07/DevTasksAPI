53b79861ee385558dfd593415bb8cd51fd5a9823
RickDev <henriiisilva2007@gmail.com>
2026-07-31 21:38:51 -0300

    Criacao do UserRepository com funcao findByLogin

---
+++ src/main/java/com/rickdev/DevTasks/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rickdev.DevTasks.Repository;
+
+import com.rickdev.DevTasks.Model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    UserDetails findByLogin(String login);
+}
